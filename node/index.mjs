6a829c4cc9c3ecd5b5dc67de2567280779901aac
houjinlong <houjinlong2014@xiaochuankeji.cn>
2022-11-22 15:44:27 +0800

    youtube

--- node/index.mjs
+++ node/index.mjs
@@ -17,6 +17,7 @@ app.use(koaBody({
         maxFileSize: 20000 * 1024 * 1024
     }
 }))
+// spotify
 const has = (id)=>{
     if(fs.existsSync(`./output/${id}.mp3`)){
         return {
@@ -35,6 +36,7 @@ const has = (id)=>{
 let tempMap = {
 
 }
+
 router.get('/spotify/start', (ctx) => {
     const {id} = ctx.request.query
     const {status} = has(id)
@@ -54,15 +56,36 @@ router.get('/spotify/start', (ctx) => {
     }
     ctx.body = {}
 });
-
+// spotify + youtube
+router.post('/youtube/start', (ctx) => {
+    const {id,spotify,youtube} = ctx.request.body
+    const {status} = has(id)
+    if(status===0&&!tempMap[id]){
+        tempMap[id] = true
+        execa("spotdl",[`"${youtube}|${spotify}"`,'--use-youtube','--ffmpeg','../ffmpeg-5.0-amd64-static/ffmpeg','--path-template',`./output/${id}.{ext}`]).then(result =>{
+            if(result.stdout.indexOf('Done')!==-1){
+                console.log(new Date(),'成功')
+            }else{
+                fs.writeFileSync(`./error/${id}`,result.stdout)
+            }
+            delete tempMap[id]
+        }).catch(err=>{
+            delete tempMap[id]
+            fs.writeFileSync(`./error/${id}`,err.toString())
+        });
+    }
+    ctx.body = {}
+});
 router.get('/spotify/status', (ctx) => {
     const {id} = ctx.request.query
     ctx.body = has(id)
 });
 
+
 app.use(router.routes()).use(router.allowedMethods());
 // 静态服务录音文件 ${DOMAIN}/file/
 app.use(serve({rootDir: './output',rootPath: '/file'}))
+app.use(serve({rootDir: './html',rootPath: '/'}))
 app.listen(process.env.PORT, () => {
     console.log(`HTTPS Server is running on: ${process.env.DOMAIN}`)
 })
--- node/pm2.config.js
+++ node/pm2.config.js
@@ -4,7 +4,8 @@ module.exports = {
     "script": "./index.mjs",
     "cwd": "./",
     "env":{
-      DOMAIN:'http://82.156.51.38:10092',
+      DOMAIN:'http://127.0.0.1:10092',
+      // DOMAIN:'http://82.156.51.38:10092',
       PORT:'10092'
     }
   }
